47c1f6e545eb9067a2476e7e38f38796e18d4081
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2024-09-03 14:47:58 +0300

    Initial Commit

---
+++ Sources/MathLibrary/MathOperations.swift
@@ -0,0 +1,41 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ali Çolak on 3.09.2024.
+//
+
+import Foundation
+
+public struct MathOperations {
+    
+    // Toplama işlemi
+    public static func add(_ a: Double, _ b: Double) -> Double {
+        return a + b
+    }
+    
+    // Çıkarma işlemi
+    public static func subtract(_ a: Double, _ b: Double) -> Double {
+        return a - b
+    }
+    
+    // Çarpma işlemi
+    public static func multiply(_ a: Double, _ b: Double) -> Double {
+        return a * b
+    }
+    
+    // Bölme işlemi
+    public static func divide(_ a: Double, _ b: Double) -> Double? {
+        guard b != 0 else {
+            print("Hata: Bölme sıfıra yapılamaz.")
+            return nil
+        }
+        return a / b
+    }
+    
+    // Üs alma işlemi
+    public static func power(_ base: Double, _ exponent: Double) -> Double {
+        return pow(base, exponent)
+    }
+}
+
